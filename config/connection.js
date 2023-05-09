09e5a35a7619fcb0ade323d873a824214e4f53f6
Joseph Baek <josephbaek97@gmail.com>
2023-05-08 21:37:41 -0700

    Still needs controllers

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const {connect, connection} = requre('mongoose');
+
+const connectionString = process.env.MONGODB_URI || 'mongodb: //localhost:27017/socialNetworkDB';
+
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = connection; 
